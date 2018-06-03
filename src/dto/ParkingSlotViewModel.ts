9a9422de6babffdb72441a7cf7d82811232e1ec9
hreddy <hreddy@BLR-L-PG00V793>
2018-06-03 23:00:52 +0530

    API Layer creation and API Integrations

---
+++ src/dto/ParkingSlotViewModel.ts
@@ -0,0 +1,9 @@
+export class ParkingSlotViewModel{
+    public Id:number;
+    public IsOccupied:boolean;
+    public UserId:number;
+    public UserId123:number;
+    public Location:string;
+    public Level :number;
+    public SequenceOrder:number;
+}
